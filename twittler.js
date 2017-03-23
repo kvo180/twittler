e8f721b73a7dc553d6aa9f054915547cefb6b384
kvo180 <khoavo180@gmail.com>
2017-03-22 17:57:59 -0700

    Update styles and move timestamps into separate p

--- twittler.js
+++ twittler.js
@@ -7,11 +7,15 @@ function getTweets() {
     var tweet = streams.home[index];
     var $tweet = $('<div class="tweet"></div>');
     var $userLink = $('<span class="userlink" id=' +index+ '>@' +tweet.user+ '</span>');
-    var $message = $('<p></p>');
+    var $message = $('<p class="message"></p>');
+    var $timestamp = $('<p class="timestamp"></p>');
 
-    $message.text(': ' + tweet.message + ' ' + formatDate(tweet.created_at));
+    $message.text(': ' + tweet.message);
+    $timestamp.text(formatDate(tweet.created_at));
+
     $userLink.prependTo($message);
     $message.appendTo($tweet);
+    $timestamp.appendTo($tweet);
     $tweet.appendTo($main);
     index -= 1;
   }
@@ -24,10 +28,15 @@ function getTweetsForUser(username) {
 
 	while (index >= 0) {
 		var tweet = streams.users[username][index]
-		var userMessage = tweet.message;
 		var $userTweet = $('<div class="tweet"></div>');
-		$userTweet.text('@' + username + ': ' + userMessage + ' ' + formatDate(tweet.created_at));
+		var $message = $('<p class="message"></p>');
+		var $timestamp = $('<p class="timestamp"></p>');
 
+		$message.text('@' + username + ': ' + tweet.message);
+		$timestamp.text(formatDate(tweet.created_at));
+
+		$message.appendTo($userTweet);
+		$timestamp.appendTo($userTweet);
 		$userTweet.appendTo($('main'));
 		index -= 1;
 	}
@@ -40,15 +49,11 @@ $(document).ready(function(){
   	$('h4').text('created by Khoa Vo');
   	$('.btn').text('get new tweets!');
   	getTweets();
-  	console.log('get more tweets');
   });
 
   $('main').on('click', '.userlink', function() {
   	var username = $(this).text().substring(1);
-  	console.log('clicked on user');
-  	// $('h1').css('cursor', 'pointer');
 		$('h4').text($(this).text());
-		// $('.btn').hide();
 		$('.btn').text('home');
 		getTweetsForUser(username);
   });
